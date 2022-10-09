fcba1960c7dc81380909e44f414d1c7b182b8611
kimeu254 <jimkimeu254@gmail.com>
2022-10-09 12:22:06 +0300

    second commit

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// ----- STRINGS ------
+
+const baseURL = 'http://192.168.43.109:8000/api';
+const loginURL = baseURL + '/login';
+const registerURL = baseURL + '/register';
+const logoutURL = baseURL + '/logout';
+const userURL = baseURL + '/user';
+const postsURL = baseURL + '/posts';
+const commentsURL = baseURL + '/comments';
+
+// ----- Errors -----
+const serverError = 'Server error';
+const unauthorized = 'Unauthorized';
+const somethingWentWrong = 'Something went wrong, try again!';
