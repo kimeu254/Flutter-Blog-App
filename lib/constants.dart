98799278118d53125573cef329076c9698a73fa4
kimeu254 <jimkimeu254@gmail.com>
2022-10-10 19:30:20 +0300

    third commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -14,3 +14,47 @@ const commentsURL = baseURL + '/comments';
 const serverError = 'Server error';
 const unauthorized = 'Unauthorized';
 const somethingWentWrong = 'Something went wrong, try again!';
+
+// ----- Input decoration ---
+InputDecoration kInputDecoration(String label) {
+  return InputDecoration(
+    labelText: label,
+    contentPadding: EdgeInsets.all(10),
+    border: OutlineInputBorder(
+        borderSide: BorderSide(width: 1, color: Colors.black)),
+  );
+}
+
+// ----- Button ------
+TextButton kTextButton(String label, Function onPressed) {
+  return TextButton(
+    child: Text(
+      label,
+      style: TextStyle(color: Colors.white),
+    ),
+    style: ButtonStyle(
+        backgroundColor:
+            MaterialStateColor.resolveWith((states) => Colors.blue),
+        padding: MaterialStateProperty.resolveWith(
+            (states) => EdgeInsets.symmetric(vertical: 20))),
+    onPressed: () => onPressed(),
+  );
+}
+
+// ----- Login-Register hint -----
+
+Row kLoginRegisterHint(String text, String label, Function onTap) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(text),
+      GestureDetector(
+        child: Text(
+          label,
+          style: TextStyle(color: Colors.blue),
+        ),
+        onTap: () => onTap(),
+      )
+    ],
+  );
+}
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/api_response.dart';
+import 'package:flutter_application_1/models/user.dart';
+import 'package:flutter_application_1/services/user_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../constants.dart';
+import 'home.dart';
+import 'register.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,8 +15,84 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  TextEditingController txtEmail = TextEditingController();
+  TextEditingController txtPassword = TextEditingController();
+
+  void _loginUser() async {
+    ApiResponse response = await login(txtEmail.text, txtPassword.text);
+    if (response.error == null) {
+      _saveAndRedirectToHome(response.data as User);
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('${response.error}')));
+    }
+  }
+
+  void _saveAndRedirectToHome(User user) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    await pref.setString('token', user.token ?? '');
+    await pref.setInt('userId', user.id ?? 0);
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => Home()), (route) => false);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        centerTitle: true,
+      ),
+      body: Form(
+        key: formkey,
+        child: ListView(
+          padding: EdgeInsets.all(32),
+          children: [
+            TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                controller: txtEmail,
+                validator: (val) =>
+                    val!.isEmpty ? 'Invalid email address' : null,
+                decoration: kInputDecoration('Email')),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+                controller: txtPassword,
+                obscureText: true,
+                validator: (val) => val!.isEmpty ? 'Required min 6 char' : null,
+                decoration: kInputDecoration('Password')),
+            SizedBox(
+              height: 10,
+            ),
+            TextButton(
+              child: Text(
+                'Login',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateColor.resolveWith((states) => Colors.blue),
+                  padding: MaterialStateProperty.resolveWith(
+                      (states) => EdgeInsets.symmetric(vertical: 10))),
+              onPressed: () {
+                if (formkey.currentState!.validate()) {
+                  _loginUser();
+                }
+              },
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            kLoginRegisterHint('Dont have an account?', 'Register', () {
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) => Register()),
+                  (route) => false);
+            })
+          ],
+        ),
+      ),
+    );
   }
 }
