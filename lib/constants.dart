5a0367e5532f59ca67368bfd4bd672a2aaa6645f
kimeu254 <jimkimeu254@gmail.com>
2022-10-20 15:36:37 +0300

    added like and comment icons and counter

--- lib/constants.dart
+++ lib/constants.dart
@@ -58,3 +58,32 @@ Row kLoginRegisterHint(String text, String label, Function onTap) {
     ],
   );
 }
+
+// likes and comments
+
+Expanded kLikeAndComment(
+    int value, IconData icon, Color color, Function onTap) {
+  return Expanded(
+      child: Material(
+    child: InkWell(
+      onTap: () => onTap(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 16,
+              color: color,
+            ),
+            SizedBox(
+              width: 4,
+            ),
+            Text('$value')
+          ],
+        ),
+      ),
+    ),
+  ));
+}
